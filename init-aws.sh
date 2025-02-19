eae92e4ba826d40de83d9515ad6462cd7f0a1930
Steffi Grieger <stephanie.grieger@factorypal.com>
2025-02-19 16:37:25 +0100

    DT-1171, DT-1176 prepare glue environment

--- init-aws.sh
+++ init-aws.sh
@@ -5,3 +5,9 @@ awslocal s3 mb s3://my-bucket
 
 echo "Put example file"
 awslocal s3 cp /etc/localstack/example.parquet s3://my-bucket/
+
+# todo: finish
+echo "Initialize localstack glue"
+awslocal glue create-database --database-input '{"Name":"db1"}'
+awslocal glue create-table --database db1 --table-input '{"Name":"table1"}'
+awslocal glue get-tables --database db1
